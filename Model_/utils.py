9c2406c9fe2c1b5cd327a1ee41883cc86c6d5bef
BonianLu <LBNphoenix7@gmail.com>
2021-06-26 23:07:34 -0500

    Test

--- Model_/utils.py
+++ Model_/utils.py
@@ -1,13 +1,3 @@
-import torch
-
-
-def idx2onehot(idx, n):
-
-    assert torch.max(idx).item() < n
-    if idx.dim() == 1:
-        idx = idx.unsqueeze(1)
-
-    onehot = torch.zeros(idx.size(0), n)
-    onehot.scatter_(1, idx, 1)
-
-    return onehot
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8e2fe9cc04326604a4270c07c265e620cb6407416462dbc92cca77aed6d1dd9
+size 223
